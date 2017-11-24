0f2781b61ee6c40b54fa3f81f4eddc866041f7b0
gelther <lpw.sormano@gmail.com>
2017-11-24 21:21:05 +0100

    Code consistency changes for includes/widgets/class-wc-widget-cart.php

--- includes/widgets/class-wc-widget-cart.php
+++ includes/widgets/class-wc-widget-cart.php
@@ -22,11 +22,11 @@ class WC_Widget_Cart extends WC_Widget {
 	 */
 	public function __construct() {
 		$this->widget_cssclass    = 'woocommerce widget_shopping_cart';
-		$this->widget_description = __( "Display the customer shopping cart.", 'woocommerce' );
+		$this->widget_description = __( 'Display the customer shopping cart.', 'woocommerce' );
 		$this->widget_id          = 'woocommerce_widget_cart';
 		$this->widget_name        = __( 'Cart', 'woocommerce' );
 		$this->settings           = array(
-			'title'  => array(
+			'title'         => array(
 				'type'  => 'text',
 				'std'   => __( 'Cart', 'woocommerce' ),
 				'label' => __( 'Title', 'woocommerce' ),
@@ -71,4 +71,5 @@ class WC_Widget_Cart extends WC_Widget {
 
 		$this->widget_end( $args );
 	}
+
 }
