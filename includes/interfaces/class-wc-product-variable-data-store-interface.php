731c1bd5afbbc40674b882b39a6d8a33748d499e
gelther <lpw.sormano@gmail.com>
2017-11-24 21:18:23 +0100

    Code consistency changes for includes/interfaces/class-wc-product-variable-data-store-interface.php

--- includes/interfaces/class-wc-product-variable-data-store-interface.php
+++ includes/interfaces/class-wc-product-variable-data-store-interface.php
@@ -16,7 +16,7 @@ interface WC_Product_Variable_Data_Store_Interface {
 	/**
 	 * Does a child have a weight set?
 	 *
-	 * @param WC_Product
+	 * @param  WC_Product
 	 * @return boolean
 	 */
 	public function child_has_weight( $product );
@@ -24,7 +24,7 @@ interface WC_Product_Variable_Data_Store_Interface {
 	/**
 	 * Does a child have dimensions set?
 	 *
-	 * @param WC_Product
+	 * @param  WC_Product
 	 * @return boolean
 	 */
 	public function child_has_dimensions( $product );
@@ -32,7 +32,7 @@ interface WC_Product_Variable_Data_Store_Interface {
 	/**
 	 * Is a child in stock?
 	 *
-	 * @param WC_Product
+	 * @param  WC_Product
 	 * @return boolean
 	 */
 	public function child_is_in_stock( $product );
@@ -41,8 +41,8 @@ interface WC_Product_Variable_Data_Store_Interface {
 	 * Syncs all variation names if the parent name is changed.
 	 *
 	 * @param WC_Product $product
-	 * @param string $previous_name
-	 * @param string $new_name
+	 * @param string     $previous_name
+	 * @param string     $new_name
 	 * @since 3.0.0
 	 */
 	public function sync_variation_names( &$product, $previous_name = '', $new_name = '' );
@@ -65,7 +65,7 @@ interface WC_Product_Variable_Data_Store_Interface {
 	/**
 	 * Delete variations of a product.
 	 *
-	 * @param int $product_id
+	 * @param int  $product_id
 	 * @param bool $force_delete False to trash.
 	 */
 	public function delete_variations( $product_id, $force_delete = false );
