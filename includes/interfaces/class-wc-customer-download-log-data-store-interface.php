b366fbe7036a028a9864c742d11a594f574ebc21
gelther <lpw.sormano@gmail.com>
2017-11-24 21:18:02 +0100

    Code consistency changes for includes/interfaces/class-wc-customer-download-log-data-store-interface.php

--- includes/interfaces/class-wc-customer-download-log-data-store-interface.php
+++ includes/interfaces/class-wc-customer-download-log-data-store-interface.php
@@ -16,14 +16,14 @@ interface WC_Customer_Download_Log_Data_Store_Interface {
 	 * Get array of download log ids by specified args.
 	 *
 	 * @param  array $args
-	 * @return array of WC_Customer_Download_Log
+	 * @return array       of WC_Customer_Download_Log
 	 */
 	public function get_download_logs( $args = array() );
 
 	/**
 	 * Get logs for a specific download permission.
 	 *
-	 * @param  int $permission_id
+	 * @param  int   $permission_id
 	 * @return array
 	 */
 	public function get_download_logs_for_permission( $permission_id );
