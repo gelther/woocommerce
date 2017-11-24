5a63ab99a4870450928c7a13131b4b37c767daf5
gelther <lpw.sormano@gmail.com>
2017-11-24 21:17:53 +0100

    Code consistency changes for includes/interfaces/class-wc-abstract-order-data-store-interface.php

--- includes/interfaces/class-wc-abstract-order-data-store-interface.php
+++ includes/interfaces/class-wc-abstract-order-data-store-interface.php
@@ -18,7 +18,7 @@ interface WC_Abstract_Order_Data_Store_Interface {
 	 * Read order items of a specific type from the database for this order.
 	 *
 	 * @param  WC_Order $order
-	 * @param  string $type
+	 * @param  string   $type
 	 * @return array
 	 */
 	public function read_items( $order, $type );
@@ -27,14 +27,14 @@ interface WC_Abstract_Order_Data_Store_Interface {
 	 * Remove all line items (products, coupons, shipping, taxes) from the order.
 	 *
 	 * @param WC_Order $order
-	 * @param string $type Order item type. Default null.
+	 * @param string   $type  Order item type. Default null.
 	 */
 	public function delete_items( $order, $type = null );
 
 	/**
 	 * Get token ids for an order.
 	 *
-	 * @param WC_Order $order
+	 * @param  WC_Order $order
 	 * @return array
 	 */
 	public function get_payment_token_ids( $order );
@@ -43,7 +43,7 @@ interface WC_Abstract_Order_Data_Store_Interface {
 	 * Update token ids for an order.
 	 *
 	 * @param WC_Order $order
-	 * @param array $token_ids
+	 * @param array    $token_ids
 	 */
 	public function update_payment_token_ids( $order, $token_ids );
 }
