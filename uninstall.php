aa09e113ab152e587ec9bb2b5686b2566fceb302
gelther <lpw.sormano@gmail.com>
2017-11-24 21:29:17 +0100

    Code consistency changes for uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -22,7 +22,7 @@ wp_clear_scheduled_hook( 'woocommerce_cleanup_sessions' );
 wp_clear_scheduled_hook( 'woocommerce_geoip_updater' );
 wp_clear_scheduled_hook( 'woocommerce_tracker_send_event' );
 
-/*
+/**
  * Only remove ALL product and page data if WC_REMOVE_ALL_DATA constant is set to true in user's
  * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
  * and to ensure only the site owner can perform this action.
