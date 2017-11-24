cb7efc2673c70f191c08b6dac833cffc3b507490
gelther <lpw.sormano@gmail.com>
2017-11-24 21:21:53 +0100

    Code consistency changes for templates/cart/shipping-calculator.php

--- templates/cart/shipping-calculator.php
+++ templates/cart/shipping-calculator.php
@@ -101,4 +101,4 @@ do_action( 'woocommerce_before_shipping_calculator' ); ?>
 	</section>
 </form>
 
-<?php do_action( 'woocommerce_after_shipping_calculator' ); ?>
+<?php do_action( 'woocommerce_after_shipping_calculator' );
